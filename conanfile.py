d97f337fba9c16a41bc386c6091b3d591762c9a2
Icy-13 <zorinms@mail.ru>
2025-10-28 02:13:26 +0300

    init repo

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake
+
+class Recipe(ConanFile):
+    name = "BlockToC"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("pugixml/1.15")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.15]")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
